00c2bd98bc878a4c4b1e6e5a588a908c92de1c8c
Alexandre Carisey <alex.f.carisey@gmail.com>
2017-02-20 13:17:52 -0600

    Batch processor
    See content of FastTracker_batch.m

---
+++ Sample data/FastTracker_batch.m
@@ -0,0 +1,60 @@
+% FastTracker('Beads - Control.csv',0.050,'µm','s',100,1,0.25,0.75,0.25,1.5);
+% FastTracker('Beads - Control.csv',0.050,'µm','s',100,2,0.25,0.75,0.25,1.5);
+% FastTracker('Beads - Control.csv',0.050,'µm','s',100,3,0.25,0.75,0.25,1.5);
+% FastTracker('Beads - Control.csv',0.050,'µm','s',100,5,0.25,0.75,0.25,1.5);
+% FastTracker('Beads - Control.csv',0.050,'µm','s',100,10,0.25,0.75,0.25,1.5);
+% FastTracker('Beads - Control.csv',0.050,'µm','s',100,15,0.25,0.75,0.25,1.5);
+% FastTracker('Beads - Control.csv',0.050,'µm','s',100,20,0.25,0.75,0.25,1.5);
+% FastTracker('Beads - Control.csv',0.050,'µm','s',100,25,0.25,0.75,0.25,1.5);
+
+% FastTracker('Repeat 1 - NK92.csv',0.011,'µm','s',100,1,0.25,0.75,0.25,1.5);
+% FastTracker('Repeat 1 - NK92.csv',0.011,'µm','s',100,2,0.25,0.75,0.25,1.5);
+% FastTracker('Repeat 1 - NK92.csv',0.011,'µm','s',100,3,0.25,0.75,0.25,1.5);
+% FastTracker('Repeat 1 - NK92.csv',0.011,'µm','s',100,5,0.25,0.75,0.25,1.5);
+% FastTracker('Repeat 1 - NK92.csv',0.011,'µm','s',100,10,0.25,0.75,0.25,1.5);
+% FastTracker('Repeat 1 - NK92.csv',0.011,'µm','s',100,15,0.25,0.75,0.25,1.5);
+% FastTracker('Repeat 1 - NK92.csv',0.011,'µm','s',100,20,0.25,0.75,0.25,1.5);
+% FastTracker('Repeat 1 - NK92.csv',0.011,'µm','s',100,25,0.25,0.75,0.25,1.5);
+% FastTracker('Repeat 1 - NK92.csv',0.011,'µm','s',100,50,0.25,0.75,0.25,1.5);
+% FastTracker('Repeat 1 - NK92.csv',0.011,'µm','s',100,100,0.25,0.75,0.25,1.5);
+% 
+% FastTracker('Repeat 2 - NK92.csv',0.011,'µm','s',100,1,0.25,0.75,0.25,1.5);
+% FastTracker('Repeat 2 - NK92.csv',0.011,'µm','s',100,2,0.25,0.75,0.25,1.5);
+% FastTracker('Repeat 2 - NK92.csv',0.011,'µm','s',100,3,0.25,0.75,0.25,1.5);
+% FastTracker('Repeat 2 - NK92.csv',0.011,'µm','s',100,5,0.25,0.75,0.25,1.5);
+% FastTracker('Repeat 2 - NK92.csv',0.011,'µm','s',100,10,0.25,0.75,0.25,1.5);
+% FastTracker('Repeat 2 - NK92.csv',0.011,'µm','s',100,15,0.25,0.75,0.25,1.5);
+% FastTracker('Repeat 2 - NK92.csv',0.011,'µm','s',100,20,0.25,0.75,0.25,1.5);
+% FastTracker('Repeat 2 - NK92.csv',0.011,'µm','s',100,25,0.25,0.75,0.25,1.5);
+% FastTracker('Repeat 2 - NK92.csv',0.011,'µm','s',100,50,0.25,0.75,0.25,1.5);
+% FastTracker('Repeat 2 - NK92.csv',0.011,'µm','s',100,100,0.25,0.75,0.25,1.5);
+% 
+% FastTracker('Repeat 3 - YTS.csv',0.011,'µm','s',100,1,0.25,0.75,0.25,1.5);
+% FastTracker('Repeat 3 - YTS.csv',0.011,'µm','s',100,2,0.25,0.75,0.25,1.5);
+% FastTracker('Repeat 3 - YTS.csv',0.011,'µm','s',100,3,0.25,0.75,0.25,1.5);
+% FastTracker('Repeat 3 - YTS.csv',0.011,'µm','s',100,5,0.25,0.75,0.25,1.5);
+% FastTracker('Repeat 3 - YTS.csv',0.011,'µm','s',100,10,0.25,0.75,0.25,1.5);
+% FastTracker('Repeat 3 - YTS.csv',0.011,'µm','s',100,15,0.25,0.75,0.25,1.5);
+% FastTracker('Repeat 3 - YTS.csv',0.011,'µm','s',100,20,0.25,0.75,0.25,1.5);
+% FastTracker('Repeat 3 - YTS.csv',0.011,'µm','s',100,25,0.25,0.75,0.25,1.5);
+% FastTracker('Repeat 3 - YTS.csv',0.011,'µm','s',100,50,0.25,0.75,0.25,1.5);
+% FastTracker('Repeat 3 - YTS.csv',0.011,'µm','s',100,100,0.25,0.75,0.25,1.5);
+% 
+% FastTracker('Fiducial beads.csv',0.011,'µm','s',100,1,0.25,0.75,0.25,1.5);
+% FastTracker('Fiducial beads.csv',0.011,'µm','s',100,2,0.25,0.75,0.25,1.5);
+% FastTracker('Fiducial beads.csv',0.011,'µm','s',100,3,0.25,0.75,0.25,1.5);
+% FastTracker('Fiducial beads.csv',0.011,'µm','s',100,5,0.25,0.75,0.25,1.5);
+% FastTracker('Fiducial beads.csv',0.011,'µm','s',100,10,0.25,0.75,0.25,1.5);
+% FastTracker('Fiducial beads.csv',0.011,'µm','s',100,15,0.25,0.75,0.25,1.5);
+% FastTracker('Fiducial beads.csv',0.011,'µm','s',100,20,0.25,0.75,0.25,1.5);
+% FastTracker('Fiducial beads.csv',0.011,'µm','s',100,25,0.25,0.75,0.25,1.5);
+% FastTracker('Fiducial beads.csv',0.011,'µm','s',100,50,0.25,0.75,0.25,1.5);
+% FastTracker('Fiducial beads.csv',0.011,'µm','s',100,100,0.25,0.75,0.25,1.5);
+% Warning: some of the fiducial beads were acquired at a different frame rate.
+
+FastTracker('eNK - Control.csv',1,'µm','s',1,1,0.25,0.75,0.25,1.5);
+FastTracker('eNK - NDZ.csv',1,'µm','s',1,1,0.25,0.75,0.25,1.5);
+FastTracker('NK92 - Control.csv',1,'µm','s',1,1,0.25,0.75,0.25,1.5);
+FastTracker('NK92 - NDZ.csv',1,'µm','s',1,1,0.25,0.75,0.25,1.5);
+FastTracker('YTS - Control.csv',1,'µm','s',1,1,0.25,0.75,0.25,1.5);
+FastTracker('YTS - NDZ.csv',1,'µm','s',1,1,0.25,0.75,0.25,1.5);
